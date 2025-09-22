7fbfafba9d0d5ade813232bbcac838dfee1c1496
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-09-22 23:53:04 +0200

    s

--- compile.sh
+++
@@ -1,1 +0,0 @@
-python3 assemble.py program.asm --out rom_init.hex --pad 256
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-./compile.sh
+python3 assemble.py program.asm --out rom_init.hex --pad 256
 iverilog -o sim.vvp cpu.v tb.v
 vvp sim.vvp
