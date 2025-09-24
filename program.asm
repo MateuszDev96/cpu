d5fa02ca02826348b42860199be29308068a1582
MateuszDev96 <33595932+MateuszDev96@users.noreply.github.com>
2025-09-24 22:02:17 +0200

    d

--- program.asm
+++ program.asm
@@ -1,8 +1,8 @@
 LI r1, 1         ; ustaw r1 = 1
-LI r0, 5         ; r0 = licznik
+LI r0, 125        ; r0 = licznik
 .loop:
 ST  r0, 0xFF     ; wypisz r0
 SUB r0, r1       ; zmniejsz r0 o 1
-JZ  r0, 2        ; jeśli r0 == 0, przeskocz 2 instrukcje (czyli HALT)
-JMP .loop
-HALT             ; tu zakończ program
+JZ  r0, 2        ; jeśli r0 == 0, przeskocz 2 instrukcje (czyli do HALT)
+JMP .loop        ; skocz na początek pętli
+HALT             ; zatrzymaj program
--- tb.v
+++ tb.v
@@ -18,7 +18,7 @@ module tb;
 
     initial begin
         #12 rst = 0;
-        #2000 $finish;
+        #3000000 $finish;  // symulacja potrwa 3 sekundy
     end
 
     reg prev = 0;
